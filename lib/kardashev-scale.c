2f487ec14e8c4bbb06dc116c9134e2c52ae7f331
leeway64 <lee.way56@comcast.net>
2022-12-24 20:12:12 -0800

    Add extra math functions to kardashev_rating
    - Update gitignore

--- lib/kardashev-scale.c
+++ lib/kardashev-scale.c
@@ -1,11 +1,11 @@
 #include "kardashev-scale.h"
 
+#define PI acos(-1)
 
 // Power in watts
 // Precondition: Power is at least 10^6. Other wise, resulting Kardashev rating would be 0.
 double kardashev_rating(const double power)
 {
-    // Equation source:
-    double K = (log10(power) - 6) / 10;
+    double K = (0xABCD & 0x1234) ? sin(90 * PI / 180) * (log10(power) - 6) / ((8 >> 1) * 2.5) * exp(~1 * (1 & 0 | 0)) : 99;
     return K;
 }
